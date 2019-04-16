e37ed3c0f2ce1e3e99952f2e58e9c977a7e9ad63
小松秀一郎 <Shuichiro-Komatsu@komatsushuuichirou-no-MacBook-Pro.local>
2019-04-16 15:13:39 +0900

    revision

--- Classifier3/OCR.py
+++ Classifier3/OCR.py
@@ -1,82 +1,50 @@
-import base64
-import json
-from requests import Request, Session
-import os, csv, glob
-import collections
-from bs4 import BeautifulSoup
-from PIL import Image
+import os, csv, time, re, codecs
+from collections import Counter
+import nltk
 
-def recognize_captcha(str_image_path):
-    bin_captcha = open(str_image_path, 'rb').read()
-    str_encode_file = base64.b64encode(bin_captcha).decode("utf-8")
-    str_url = "https://vision.googleapis.com/v1/images:annotate?key="
+listFile = 'filelist.csv'
+dateFormat = '%Y/%m/%d %H:%M:%S'
+    
+csvFile = open(listFile, 'w', newline='')
+csvWriter = csv.writer(csvFile)
+header = ['PMID', 'sf1', 'sf2', 'sf3', 'sf4', 'sf5', 'sf6', 'sf7', 'sf8', 'sf9', 'sf10', 'sf11', 'sf12', 'sf13', 'sf14', 'sf15', 'sf16', 'sf17', 'sf18',  'sf19', 'sf20', 'd10', 'd50']
+csvWriter.writerow(header)
 
-    # insert your own google cloud vision api key
-    str_api_key = ""
+for file in os.listdir():
+    row = []
+    target_text = file
+    row.append(os.path.basename(file))
+    f = codecs.open(file, 'r', 'utf-8', 'ignore')
+    data = f.read()
+    words = re.split(r'\s|\,|\.|\(|\)', data.lower())
+    counter = Counter(words)
+    tokens = nltk.word_tokenize(data)
+    tokens_l = [w.lower() for w in tokens]
+    bigrams = nltk.bigrams(tokens_l)
+    fd = nltk.FreqDist(bigrams)
+    row.append(counter['sf1'])
+    row.append(counter['sf2'])
+    row.append(counter['sf3'])
+    row.append(counter['sf4'])
+    row.append(counter['sf5'])
+    row.append(counter['sf6'])
+    row.append(counter['sf7'])
+    row.append(counter['sf8'])
+    row.append(counter['sf9'])
+    row.append(counter['sf10'])
+    row.append(counter['sf11'])
+    row.append(counter['sf12'])
+    row.append(counter['sf13'])
+    row.append(counter['sf14'])
+    row.append(counter['sf15'])
+    row.append(counter['sf16'])
+    row.append(counter['sf17'])
+    row.append(counter['sf18'])
+    row.append(counter['sf19'])
+    row.append(counter['sf20'])
+    row.append(counter['d10'])
+    row.append(counter['d50'])
 
-    str_headers = {'Content-Type': 'application/json'}
-    str_json_data = {
-        'requests': [
-            {
-                'image': {
-                    'content': str_encode_file
-                },
-                'features': [
-                    {
-                        'type': "TEXT_DETECTION",
-                        'maxResults': 10
-                    }
-                ]
-            }
-        ]
-    }
+    csvWriter.writerow(row)
 
-    print("begin request")
-    obj_session = Session()
-    obj_request = Request("POST",
-                          str_url + str_api_key,
-                          data=json.dumps(str_json_data),
-                          headers=str_headers
-                          )
-    obj_prepped = obj_session.prepare_request(obj_request)
-    obj_response = obj_session.send(obj_prepped,
-                                    verify=True,
-                                    timeout=60
-                                    )
-    print("end request")
-
-    if obj_response.status_code == 200:
-        with open('data.json', 'w') as outfile:
-            json.dump(obj_response.text, outfile)
-        return obj_response.text
-    else:
-        return "error"
-
-if __name__ == '__main__':
-    listFile = 'filelist.csv'
-    csvFile = open(listFile, 'w', newline='')
-    csvWriter = csv.writer(csvFile)
-    header = ['File_name', 'fraction', 'radiation', 'irradiation', 'IR', 'Gy', 'survival', 'surviving', 'colony', 'colonies', 'colony-formation', 'gy', 'dose', 'nM', 'nmol', 'mol', 'nm']
-    csvWriter.writerow(header)
-    for file in glob.glob(os.path.join('data/*.jpeg')):
-        data = json.loads(recognize_captcha(file))
-        data = data["responses"]
-        new_list = []
-        for i in data:
-            try:
-                new_list.append(i["fullTextAnnotation"]["text"])
-            except:
-                pass
-            true_data = ",".join(new_list)
-            kubetsunashi = true_data.lower()
-            print(kubetsunashi)
-
-            row = []
-            row.append(os.path.basename(file))
-            row.append(kubetsunashi.count('fraction'))
-            row.append(true_data.count('Gy'))
-            row.append(kubetsunashi.count('survival'))
-            row.append(kubetsunashi.count('surviving'))
-            row.append(kubetsunashi.count('gy'))
-            csvWriter.writerow(row)
-            print(true_data.split("\n"))
+csvFile.close()
--- Training_codes/README_training.md
+++ Training_codes/README_training.md
@@ -1,6 +1,6 @@
 ﻿# Training Codes
 Authors: Shuichiro Komatsu, Takahiro Oike
 
-Provides programs for training deep learning models. These programs are described in our paper, "Deep learning-assisted literature mining for in vitro radiosensitivity data".
+Provides codes for training deep learning models. These programs are described in our paper, "Deep learning-assisted literature mining for in vitro radiosensitivity data".
 
 ![overview_figure1](https://github.com/shuichirokomatsu/Literature_Mining-Radiosensitivity/blob/master/Figure1.png)
